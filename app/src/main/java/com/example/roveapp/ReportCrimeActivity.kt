e4b7185776a81d5a1b67cf45fe9ec538e833adb6
Vaibhav <vaibhavmehra1119@gmail.com>
2022-04-04 23:22:14 +0530

    Report Crime Updates and select image feature turned on

--- app/src/main/java/com/example/roveapp/ReportCrimeActivity.kt
+++ app/src/main/java/com/example/roveapp/ReportCrimeActivity.kt
@@ -1,17 +1,21 @@
 package com.example.roveapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.webkit.MimeTypeMap
 import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import com.example.roveapp.databinding.ActivityReportCrimeBinding
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.roveapp.databinding.ActivityReportCrimeBinding
 import com.google.firebase.firestore.FirebaseFirestore
+import java.io.File
+
 
 class ReportCrimeActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -21,6 +25,10 @@ class ReportCrimeActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var et : EditText
     private lateinit var rb: RadioGroup
     private lateinit var rg: RadioButton
+    private lateinit var select_image: Button
+    private lateinit var upload_image:Button
+    private var SELECT_IMAGE = 1
+    private lateinit var textFile: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +38,9 @@ class ReportCrimeActivity : AppCompatActivity(), OnMapReadyCallback {
         btn= binding.button2
         et = binding.editText
         rb = binding.rg
+        select_image=binding.selectImageButton
+        upload_image=binding.uploadImageButton
+        textFile=binding.textView
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -38,6 +49,29 @@ class ReportCrimeActivity : AppCompatActivity(), OnMapReadyCallback {
             et.text.clear()
             rb.clearCheck()
             saveFireStore("26.654","80.945", rep, "L2")
+        }
+        var autotextView = findViewById<AutoCompleteTextView>(R.id.auto_complete_text)
+        val languages = resources.getStringArray(R.array.Languages)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
+        autotextView.setAdapter(adapter)
+        select_image.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "image/*"
+            startActivityForResult(intent, SELECT_IMAGE)
+        }
+        upload_image.setOnClickListener {
+            val path = textFile.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_VIEW
+            val file = File(path)
+
+            val mime = MimeTypeMap.getSingleton()
+            val ext = file.name.substring(file.name.indexOf(".") + 1)
+            val type = mime.getMimeTypeFromExtension(ext)
+
+            intent.setDataAndType(Uri.fromFile(file),type)
+
+            startActivity(intent)
         }
     }
 
@@ -55,6 +89,16 @@ class ReportCrimeActivity : AppCompatActivity(), OnMapReadyCallback {
             println("ERROR!!!!!")
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            SELECT_IMAGE -> if (resultCode === RESULT_OK) {
+                val FilePath: String = data?.data?.path.toString()
+                textFile.text = FilePath
+            }
+        }
     }
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
